491cad54f05dabae1beb4549a1f1960da189cbba
Aditi <aditipandeya1608@gmail.com>
2023-05-27 22:10:39 +0530

    scripting left

---
+++ Assets/Scripts/SetNavigationTarget.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetNavigationTarget : MonoBehaviour
+{
+    [SerializeField]
+    private Camera topDownCamera;
+    [SerializeField]
+    private GameObject navTargetObject;
+
+    private UnityEngine.AI.NavMeshPath path; //current calculated path
+    private LineRenderer line;
+
+    private bool lineToggle = false;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        path = new UnityEngine.AI.NavMeshPath();
+        line = transform.GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began)){
+            lineToggle = !lineToggle;
+        }
+        if(lineToggle){
+            UnityEngine.AI.NavMesh.CalculatePath(transform.position, navTargetObject.transform.position, UnityEngine.AI.NavMesh.AllAreas, path);
+            line.positionCount = path.corners.Length;
+            line.SetPositions(path.corners);
+            line.enables=true;
+        }
+    }
+}
